7258181f027485f1bfc556633acc3dc59fab4f64
Rafaellima1412 <rafaellima1412@gmail.com>
2024-05-28 20:32:41 -0400

    feat: initial config

---
+++ pycpaysimplificado/src/test/java/com/example/pycpaysimplificado/PycpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pycpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PycpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
